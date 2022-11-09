eddb82629c93a619ab6f57ca9f104a241988ebb4
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-11-09 23:02:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const connectToWA = () => {
 				//......................................................Commands..............................................................\\
 
 				case '6-underground-2019':
-				case '6underground2019': {
+				case '6underground': {
 
 					if (!isSUB) return
 
@@ -91,7 +91,7 @@ const connectToWA = () => {
 					break
 
 				case '12-strong-2018':
-				case '12strong2018': {
+				case '12strong': {
 
 					if (!isSUB) return
 
@@ -107,7 +107,7 @@ const connectToWA = () => {
 					break
 
 				case 'a-wrinkle-in-time-2018':
-				case 'awrinkleintime2018': {
+				case 'awrinkleintime': {
 
 					if (!isSUB) return
 
@@ -122,9 +122,259 @@ const connectToWA = () => {
 				}
 					break
 
+				case 'after':
+				case 'after-2019':
+				case 'after-we-collided-2020':
+				case 'after-we-fell-2021': {
+
+					if (!isSUB) return
+
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/After_2019_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro After 2019 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/After_We_Collided_2020_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro After We Collided 2020 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/After_We_Fell_2021_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro After We Fell 2021 .mkv'
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
+				case 'alienvspredator':
+				case 'alien-vs-predator-2004':
+				case 'aliens-vs-predator-requiem-2007': {
+
+					if (!isSUB) return
+
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/Alien_Vs_Predator_2004_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Alien Vs Predator 2004 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/Aliens_Vs_Predator_Requiem_2007_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Aliens Vs Predator Requiem 2007 .mkv'
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
+				case 'aliens-in-the-attic-2009':
+				case 'aliensintheattic': {
+
+					if (!isSUB) return
+
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/Aliens_In_The_Attic_2009_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Aliens In The Attic 2009 .mkv'
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
+				case 'americanpie':
+				case 'american-pie-1999':
+				case 'american-pie-2001':
+				case 'american-pie-the-wedding-2003':
+				case 'american-pie-presents-band-camp-2005':
+				case 'american-pie-presents-the-naked-mile-2006':
+				case 'american-pie-presents-beta-house-2007':
+				case 'american-pie-presents-the-book-of-love-2009':
+				case 'american-pie-reunion-2012':
+				case 'american-pie-presents-girls-rules-2020': {
+
+					if (!isSUB) return
+
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/American_Pie_1999_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro American Pie 1999 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/American_Pie_2001_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro American Pie 2001 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/American_Pie_The_Wedding_2003_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro American Pie The Wedding 2003 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/American_Pie_Presents_Band_Camp_2005_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro American Pie Presents Band Camp 2005 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/American_Pie_Presents_The_Naked_Mile_2006_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro American Pie Presents The Naked Mile 2006 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/American_Pie_Presents_Beta_House_2007_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro American Pie Presents Beta House 2007 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/American_Pie_Presents_The_Book_Of_Love_2009_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro American Pie Presents The Book Of Love 2009 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/American_Pie_Reunion_2012_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro American Pie Reunion 2012 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/American_Pie_Presents_Girls_Rules_2020_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro American Pie Presents Girls Rules 2020 .mkv'
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
+				case 'angrybirds':
+				case 'angry-birds-movie-2016':
+				case 'angry-birds-movie-2019': {
+
+					if (!isSUB) return
+
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/Angry_Birds_Movie_2016_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Angry Birds Movie 2016 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/Angry_Birds_Movie_2019_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Angry Birds Movie 2019 .mkv'
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
+				case 'antman':
+				case 'Ant-Man_And_The_Wasp_2018': {
+
+					if (!isSUB) return
+
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/Ant_Man_And_The_Wasp_2018_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Ant Man And The Wasp 2018 .mkv'
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
+				case 'artemis-fowl-2020':
+				case 'artemisfowl': {
+
+					if (!isSUB) return
+
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/Artemis_Fowl_2020_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Artemis Fowl 2020 .mkv'
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
+				case 'assassinscreed':
+				case 'assassins-creed-2016': {
+
+					if (!isSUB) return
+
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					await conn.sendMessage(from, {
+						document: { url: `https://cloud.nadith.pro/en_mv/Assassin's_Creed_2016_@nadithpro.mkv` },
+						mimetype: config.MKVTYPE,
+						fileName: `@nadithpro Assassin's Creed 2016 .mkv`
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
+				case 'avengers':
+				case 'the-avengers-2012':
+				case 'avengers-age-of-ultron-2015':
+				case 'avengers-infinity-war-2018':
+				case 'avengers-endgame-2019': {
+
+					if (!isSUB) return
+
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/The_Avengers_2012_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro The Avengers 2012 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/Avengers_Age_Of_Ultron_2015_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Avengers Age Of Ultron 2015 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/Avengers_Infinity_War_2018_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Avengers Infinity War 2018 .mkv'
+					})
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/Avengers_Endgame_2019_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Avengers Endgame 2019 .mkv'
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
+				case 'axl-2018':
+				case 'axl': {
+
+					if (!isSUB) return
+
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					await conn.sendMessage(from, {
+						document: { url: 'https://cloud.nadith.pro/en_mv/AXL_2018_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro AXL 2018 .mkv'
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
 				
 
-					
+
 
 
 				default:
